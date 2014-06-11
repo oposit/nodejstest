7cfb6a05444cf78f622bcf347988e51a2a8bc861
Brian Groth <bgroth@gmail.com>
2014-06-11 23:49:41 +0200

    Removed logging

--- example.js
+++ example.js
@@ -3,7 +3,7 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
-app.use(logfmt.requestLogger());
+//app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
